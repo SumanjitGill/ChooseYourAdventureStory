2f7a09d6ac3ceb51f65d713bb2a1cd7c1d9b8484
SumanjitGill <sel.suman.11@gmail.com>
2016-03-15 11:11:47 -0700

    Initial Upload
    This is the first upload of "Caroline;" present in this program is the
    class to create characters.

---
+++ CS10-SumanjitGill-Caroline.py
@@ -0,0 +1,12 @@
+#Storyline
+class Character():
+    def __init__(self, name, age, background, personality, charisma):
+        self.name = name
+        self.age = age
+        self.background = background
+        self.personality = personality
+        self.charisma = charisma
+        
+Caroline = 1
+Kyle = 2
+Gabriela = 3
