3e9ed51536e3ac34c439559206727b8bd8f62797
SumanjitGill <sel.suman.11@gmail.com>
2016-03-18 11:46:14 -0700

    Movement
    This version of the game has the movement function from room to room,
    however the edit is yet to be made about informing a player if they
    can't go a direction they typed in.

--- CS10-SumanjitGill-Caroline.py
+++ CS10-SumanjitGill-Caroline.py
@@ -1,12 +1,46 @@
+import sys
 #Storyline
 class Character():
-    def __init__(self, name, age, background, personality, charisma):
+    def __init__(self, name, age, status, background, personality, charisma, intelligence, strength):
         self.name = name
         self.age = age
+        self.status = status
         self.background = background
         self.personality = personality
         self.charisma = charisma
         
-Caroline = 1
-Liam = 2
-Gabriela = 3
+Caroline = ('Karolina "Caroline" Ativa')
+Liam = ('Liam Youngblood')
+Gabriela = ('Gabriela Solis')
+EP = ('Estevan Antonov')
+Bri = ('Brianna Scholtz')
+Ana = ('Analiese Hanz')
+Queen_Laurenya = ('Nicole Laurenya')
+Liev = ('Liev Ativa')
+Ziva = ('Ziva Antonov')
+Arnav = ('Arnav Srivastva')
+
+node = None
+
+class Room:
+    def __init__(self, n_path, s_path, e_path, w_path, name, description):
+        self.north = n_path
+        self.south = s_path
+        self.east = e_path
+        self.west = w_path
+        self.name = name
+        self.description = description
+        
+    def move(self, direction):
+        if direction in ['q', 'quit', 'exit']:
+            sys.exit(0)
+        while True:
+            global node
+            node = globals()[getattr(self, direction)]
+            break
+            
+while True:
+    print node.name
+    print node.description
+    user_command = raw_input('> ')
+    node.move(user_command)
