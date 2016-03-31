95cc4d536e1cfcfe6dbb03628f0764b40bc2f1fb
SumanjitGill <sel.suman.11@gmail.com>
2016-03-31 11:57:59 -0700

    Dialogue
    Dialogue added

--- CS10-SumanjitGill-Caroline.py
+++ CS10-SumanjitGill-Caroline.py
@@ -1,27 +1,26 @@
 import sys
 #Storyline
 class Character():
-    def __init__(self, name, age, status, background, personality, charisma, intelligence, strength):
+    def __init__(self, name, full_name):
         self.name = name
-        self.age = age
-        self.status = status
-        self.background = background
-        self.personality = personality
-        self.charisma = charisma
+        self.full_name = full_name
+        #self.age = age
+        #self.status = status
+        #self.background = background
+        #self.personality = personality
+        #self.charisma = charisma
         
-Caroline = ('Karolina "Caroline" Ativa')
-Liam = ('Liam Youngblood')
-Gabriela = ('Gabriela Solis')
-EP = ('Estevan Antonov')
-Bri = ('Brianna Scholtz')
-Ana = ('Analiese Hanz')
-Queen_Laurenya = ('Nicole Laurenya')
-Liev = ('Liev Ativa')
-Ziva = ('Ziva Antonov')
-Arnav = ('Arnav Srivastva')
+Caroline = Character('Caroline','Karolina "Caroline" Ativa')
+Liam = Character('Liam','Liam Youngblood')
+Gabriela = Character('Gabi','Gabriela Solis')
+Andres = Character('Andres','Andres Leal')
+Bree = Character('Bree','Breanna "Bree" Scholtz')
+Nicole = Character('Nicole','Nicole Laurenya')
+Liev = Character('Liev','Liev Ativa')
+Sakshi = Character('Sakshi','Sakshi Goenka')
+Leng = Character('Leng','Leng Li')
 
-node = None
-
+'''
 class Room:
     def __init__(self, n_path, s_path, e_path, w_path, name, description):
         self.north = n_path
@@ -32,8 +31,6 @@ class Room:
         self.description = description
         
     def move(self, direction):
-        if direction in ['q', 'quit', 'exit']:
-            sys.exit(0)
         while True:
             global node
             node = globals()[getattr(self, direction)]
@@ -43,8 +40,59 @@ while True:
     print node.name
     print node.description
     user_command = raw_input('> ')
+    if user_command in ['q', 'quit', 'exit']:
+        sys.exit(0)
     if user_command in ['north','south','west','east']:
         try:
             node.move(user_command)
         except:
             print 'You can\'t go that way! Try again.'
+'''
+            
+'''class Notebook:
+    def __init__(self):
+        self.notebook = []
+        
+    def write_in_notebook(self):
+        user_note = raw_input('What would you like to write in your notebook?')
+        self.notebook.append(user_note)
+        
+    def show_entry_amount(self):
+        if len(self.notebook) ==1:
+            print 'You have 1 entry.'
+        elif len(self.notebook) ==0:
+            print 'You don\'t have any entries.'
+        else:
+            print 'You have', len(self.notebook),'entries.'
+        
+    def read_entry(self):
+        for i in enumerate(self.notebook):
+            print i
+        user_entry_choice = raw_input('Which entry would you like to read?')
+        try:
+           path = [int(user_entry_choice)]
+           print self.notebook[path]
+        except:
+            print 'Invalid Number. Try a valid number.'
+            
+your_notebook = Notebook()
+#4691 E Harvard Ave Fresno, CA 93703-2075
+'''
+
+class Dialogue:
+    def __init__(self, dialogue):
+        self.dialogue = dialogue
+        
+    def say(self,char):
+        print char.name+':','"'+self.dialogue+'"'
+        
+hello = Dialogue('Hello')
+        
+class Room:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+#Do dialogue in order this way by continously calling the dialogue 'say' function
+#Progress stories this way
+
+        
